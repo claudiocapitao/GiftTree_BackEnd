33af0c0abe02ff44259f0ccdf0a6c1caff10bbf5
clarisa12 <clarisa.abalasei@gmail.com>
2021-01-18 23:05:24 +0200

    Add db queries

---
+++ db/add_trees.sql
@@ -0,0 +1,12 @@
+use gift_tree;
+
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (1,'AusTree1','AusTree tree','Australia',100,20,'https://www.bandrtreeservices.com.au/wp-content/uploads/2020/01/Historic_Trees_1.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (2,'BraTree1','BraTree tree','Brasil',80,15,'https://3.bp.blogspot.com/-BUAP87YGdiA/VreJo0qXbxI/AAAAAAAABCs/lDl4VucfuF0/s1600/pau%2Bbrasil.JPG');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (3,'SpaTree1','SpaTree tree','Spain',40,40,'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/bd/a3/43.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (4,'CanTree1','CanTree tree','Canada',150,35,'https://static.dw.com/image/16528455_403.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (5,'NigTree1','NigTree tree','Nigeria',180,80,'https://www.britishempire.co.uk/images2/cassiatree.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (6,'AusTree2','AusTree tree','Australia',100,20,'https://www.bandrtreeservices.com.au/wp-content/uploads/2020/01/Historic_Trees_1.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (7,'BraTree2','BraTree tree','Brasil',80,15,'https://3.bp.blogspot.com/-BUAP87YGdiA/VreJo0qXbxI/AAAAAAAABCs/lDl4VucfuF0/s1600/pau%2Bbrasil.JPG');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (8,'SpaTree2','SpaTree tree','Spain',40,40,'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/bd/a3/43.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (9,'CanTree2','CanTree tree','Canada',150,35,'https://static.dw.com/image/16528455_403.jpg');
+INSERT INTO TREE(TREE_ID,TREE_NAME,TREE_SPECIES,COUNTRY,PRICE,CO2_EMISSIONS,IMAGE_URL) VALUES (10,'NigTree2','NigTree tree','Nigeria',180,80,'https://www.britishempire.co.uk/images2/cassiatree.jpg');
