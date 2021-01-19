1edf4579bbaa233ec0dfed90a4532c9d97292432
clarisa12 <clarisa.abalasei@gmail.com>
2021-01-20 00:07:44 +0200

    add price range query & async

--- routers/api.js
+++ routers/api.js
@@ -1,23 +1,31 @@
 const apiRoutes = require("express").Router();
 const { selectAll, where, and, db } = require("../db/treeQueries");
 
-apiRoutes.get("/trees", (req, res) => {
+apiRoutes.get("/trees", async (req, res) => {
     let query = selectAll;
     if (Object.keys(req.query).length) {
         query += where;
         Object.keys(req.query).forEach((key, idx) => {
-            // TODO: add logic for price ranges
             if (idx != 0) {
                 query += and;
             }
-            query += key + "=" + "'" + req.query[key] + "'";
+            if (key === "PRICE") {
+                query +=
+                    key +
+                    ">" +
+                    req.query.PRICE.min +
+                    and +
+                    key +
+                    "<" +
+                    req.query.PRICE.max;
+            } else {
+                query += key + "=" + "'" + req.query[key] + "'";
+            }
         });
     }
     console.log(query);
-    db.query(query, (err, results) => {
-        if (err) throw err; // if query fails
-        res.json(results);
-    });
+    const [rows, fields] = await db.promise().query(query);
+    res.json(rows);
 });
 
 /**
