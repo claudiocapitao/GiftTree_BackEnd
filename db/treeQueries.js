f45f19703593eac0f60b24829e1547a54ed690f0
clarisa12 <clarisa.abalasei@gmail.com>
2021-01-25 22:27:42 +0200

    add search and price query to get

--- db/treeQueries.js
+++ db/treeQueries.js
@@ -3,6 +3,16 @@ const { db } = require("./connect");
 const selectAll = "SELECT * FROM TREE";
 const where = " WHERE ";
 const and = " AND ";
-const addFilter = (type, val) => `${db.escape(type)}=${db.escape(val)}`;
+const equal = (key, val) => `${key}=${db.escape(val)}`;
+const lessOrEqual = (key, val) => `${key}<=${db.escape(val)}`;
+const moreOrEqual = (key, val) => `${key}>=${db.escape(val)}`;
 
-module.exports = { selectAll, where, and, addFilter, db };
+module.exports = {
+    selectAll,
+    where,
+    and,
+    equal,
+    db,
+    lessOrEqual,
+    moreOrEqual,
+};
--- routers/api.js
+++ routers/api.js
@@ -1,30 +1,58 @@
 const apiRoutes = require("express").Router();
-const { selectAll, where, and, db } = require("../db/treeQueries");
+const {
+    selectAll,
+    where,
+    and,
+    equal,
+    lessOrEqual,
+    moreOrEqual,
+    db,
+} = require("../db/treeQueries");
+
+class Query {
+    result = "";
+
+    add(q, type) {
+        if (!this.result.includes(where) && this.result.includes("select")) {
+            result += where;
+        }
+
+        if (type) {
+            this.result += type.toUpperCase();
+        }
+
+        this.result += q;
+    }
+}
 
 apiRoutes.get("/trees", async (req, res) => {
-    let query = selectAll;
+    const query = new Query();
+    query.add(selectAll);
+
     if (Object.keys(req.query).length) {
-        query += where;
+        query.add(where);
+
+        if (req.query.search) {
+            query.add(`TREE_NAME LIKE "%${req.query.search}%"`);
+            query.add(and);
+            delete req.query.search;
+        }
+
         Object.keys(req.query).forEach((key, idx) => {
             if (idx != 0) {
-                query += and;
+                query.add(and);
             }
             if (key === "PRICE") {
-                query +=
-                    key +
-                    ">" +
-                    req.query.PRICE.min +
-                    and +
-                    key +
-                    "<" +
-                    req.query.PRICE.max;
+                query.add(moreOrEqual(key, req.query.PRICE.min));
+                query.add(and);
+                query.add(lessOrEqual(key, req.query.PRICE.max));
             } else {
-                query += key + "=" + "'" + req.query[key] + "'";
+                query.add(equal(key, req.query[key]));
             }
         });
     }
     console.log(query);
-    const [rows, fields] = await db.promise().query(query);
+    const [rows, fields] = await db.promise().query(query.result);
     res.json(rows);
 });
 
