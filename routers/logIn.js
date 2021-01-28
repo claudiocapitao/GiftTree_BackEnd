3d189ba127f18a0919742df7c947d9f94b8befc6
Claudio Capitao <claudio.capitao@gmail.com>
2021-01-28 10:44:00 +0100

    small modification in logIn to improve response to client side.

--- routers/logIn.js
+++ routers/logIn.js
@@ -9,20 +9,21 @@ const routehandlerFunction = async (req, res) => {
 
     try {
         const resultsEmail = await queryDB.queryData(userEmailQuery);
-        console.log(resultsEmail);
 
         let canUserLogIn = false;
+        let user_id = false;
 
         if (!!resultsEmail.length) {
             if (resultsEmail[0].USER_PASSWORD == user_password) {
                 canUserLogIn = true
+                user_id = resultsEmail[0].USER_ID
             } else {
                 canUserLogIn = false;
             }
         }
 
         res.status(200);
-        res.json({ canUserLogIn, user_id: resultsEmail[0].USER_ID });
+        res.json({ canUserLogIn, user_id });
     } catch {
         (e) => {
             console.log(e);
